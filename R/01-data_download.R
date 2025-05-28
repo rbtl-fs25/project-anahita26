d4c6be10623f4d58295daab5463ad423594c5c24
anahita26 <anahita.darvish@gmail.com>
2025-05-28 10:36:32 +0000

    Update table and add figure labels

--- R/01-data_download.R
+++ R/01-data_download.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(googlesheets4)
 
 # Import data from google sheets
-survey_responses <- read_sheet("https://docs.google.com/spreadsheets/d/1FUQ9N3gHNwbgX98BTXbpDgU4MWLPs3uGeCJAsf0OLGQ/edit?resourcekey=&gid=722433670#gid=722433670")
+survey_responses <- read_sheet("https://docs.google.com/spreadsheets/d/1cjFci12yRxlCgHr1N_c6NOE5upVzh_8CKqLv03BZyt4/edit?gid=722433670#gid=722433670")
 
 # Export raw data in correct folder
 write_csv(survey_responses, "data/raw/cloting_data.csv")
